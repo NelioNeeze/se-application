1a8c34caf9804c571b0eb2c42fa8ac7e64ca6389
s78769 <V4hB8TJd3>
2019-11-13 17:01:00 +0100

    Holaa

---
+++ test/application/model/ArticleTest.java
@@ -0,0 +1,21 @@
+package application.model;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class ArticleTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(Article.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+}
